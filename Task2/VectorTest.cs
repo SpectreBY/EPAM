d2fbaddaf390523bd759c7b84c1b0011a078436d
Alexey <viarzhbouski@gmail.com>
2019-11-21 19:29:27 +0300

    добавление функционала для операций с векторами

---
+++ Task2/VectorTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VectorLib;
+
+namespace Task2
+{
+    [TestClass]
+    public class VectorTest
+    {
+        /// <summary>
+        /// Тестируемые вектора
+        /// </summary>
+        private Vector vector1 = new Vector(10, 20, 4);
+        private Vector vector2 = new Vector(5, -10, 16);
+
+        /// <summary>
+        /// Тест на сложение векторов
+        /// </summary>
+        [TestMethod]
+        public void AdditionTest()
+        {
+            Vector expect = new Vector(15, 10, 20);
+            Vector result = vector1 + vector2;
+            Assert.IsTrue(expect.X == result.X &&
+                          expect.Y == result.Y &&
+                          expect.Z == result.Z);
+        }
+
+        /// <summary>
+        /// Тест на вычитание векторов
+        /// </summary>
+        [TestMethod]
+        public void SubtractionTest()
+        {
+            Vector expect = new Vector(5, 30, -12);
+            Vector result = vector1 - vector2;
+            Assert.IsTrue(expect.X == result.X &&
+                          expect.Y == result.Y &&
+                          expect.Z == result.Z);
+
+        }
+
+        /// <summary>
+        /// Тест на умножение векторов
+        /// </summary>
+        [TestMethod]
+        public void MultiplicationTest()
+        {
+            Vector expect = new Vector(50, -200, 64);
+            Vector result = vector1 * vector2;
+            Assert.IsTrue(expect.X == result.X &&
+                          expect.Y == result.Y &&
+                          expect.Z == result.Z);
+        }
+
+        /// <summary>
+        /// Тест на деление векторов
+        /// </summary>
+        [TestMethod]
+        public void DivisionTest()
+        {
+            Vector expect = new Vector(2, -2, 0.25);
+            Vector result = vector1 / vector2;
+            Assert.IsTrue(expect.X == result.X &&
+                          expect.Y == result.Y &&
+                          expect.Z == result.Z);
+        }
+
+        /// <summary>
+        /// Тест на равенство векторов
+        /// </summary>
+        [TestMethod]
+        public void EqualsTest()
+        {
+            Vector vector1 = new Vector(4, -3, 2);
+            Vector vector2 = new Vector(4, -3, 2);
+            bool result = vector1 == vector2;
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Тест на неравенство векторов
+        /// </summary>
+        [TestMethod]
+        public void EqualsMethodTest()
+        {
+            Vector vector1 = new Vector(4, -3, 2);
+            Vector vector2 = new Vector(4, -3, 2);
+            bool result = vector1.Equals(vector2);
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Тест на неравенство векторов
+        /// </summary>
+        [TestMethod]
+        public void NotEqualsTest()
+        {
+            Vector vector1 = new Vector(4, -3, 1);
+            Vector vector2 = new Vector(7, -3, 2);
+            bool result = vector1 != vector2;
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Тест на неравенство векторов
+        /// </summary>
+        [TestMethod]
+        public void NotEqualsMethodTest()
+        {
+            Vector vector1 = new Vector(4, -3, 1);
+            Vector vector2 = new Vector(7, -3, 2);
+            bool result = vector1.Equals(vector2);
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ VectorLib/Vector.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace VectorLib
+{
+    /// <summary>
+    /// Класс вектора, с переопределенными операторами
+    /// </summary>
+    public class Vector
+    {
+        /// <summary>
+        /// поля x, y, z
+        /// </summary>
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        /// <summary>
+        /// Свойство доступа к полю х
+        /// </summary>
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        /// <summary>
+        /// Свойство доступа к полю у
+        /// </summary>
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        /// <summary>
+        /// Свойство доступа к полю z
+        /// </summary>
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        /// <summary>
+        /// Свойство для получение нулевого вектора
+        /// </summary>
+        public static Vector Zero
+        {
+            get { return new Vector(0, 0, 0); }
+        }
+
+        /// <summary>
+        /// Переопределение оператора "+" для сложения векторов 
+        /// </summary>
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            double x = vector1.X + vector2.X;
+            double y = vector1.Y + vector2.Y;
+            double z = vector1.Z + vector2.Z;
+            Vector vector3 = new Vector(x, y, z);
+            return vector3;
+        }
+
+        /// <summary>
+        /// Переопределение оператора "-" для вычитания векторов 
+        /// </summary>
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            double x = vector1.X - vector2.X;
+            double y = vector1.Y - vector2.Y;
+            double z = vector1.Z - vector2.Z;
+            Vector vector3 = new Vector(x, y, z);
+            return vector3;
+        }
+
+        /// <summary>
+        /// Переопределение оператора "*" для умножения векторов 
+        /// </summary>
+        public static Vector operator *(Vector vector1, Vector vector2)
+        {
+            double x = vector1.X * vector2.X;
+            double y = vector1.Y * vector2.Y;
+            double z = vector1.Z * vector2.Z;
+            Vector vector3 = new Vector(x, y, z);
+            return vector3;
+        }
+
+        /// <summary>
+        /// Переопределение оператора "/" для деления векторов 
+        /// </summary>
+        public static Vector operator /(Vector vector1, Vector vector2)
+        {
+            double x = vector1.X / vector2.X;
+            double y = vector1.Y / vector2.Y;
+            double z = vector1.Z / vector2.Z;
+            Vector vector3 = new Vector(x, y, z);
+            return vector3;
+        }
+
+        /// <summary>
+        /// Переопределение оператора "==" для определения равенства векторов 
+        /// </summary>
+        public static bool operator ==(Vector vector1, Vector vector2)
+        {
+            if (vector1.X == vector2.X &&
+                vector1.Y == vector2.Y &&
+                vector1.Z == vector2.Z)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Переопределение оператора "!=" для определения неравенства векторов 
+        /// </summary>
+        public static bool operator !=(Vector vector1, Vector vector2)
+        {
+            if (vector1 == vector2)
+                return false;
+            else
+                return true;
+        }
+
+        /// <summary>
+        /// Переопределение метода для определения равенства векторов  
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            if (this == (Vector)obj)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Переопределение метода для получения хеш-кода вектора 
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return this.GetHashCode();
+        }
+    }
+}
