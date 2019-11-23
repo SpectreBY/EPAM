758be8bb8479dc97b21697f21bd18af8b397abfe
Alexey <Alexey@WIN-FP6SQOTN5JC>
2019-11-24 02:23:36 +0300

    добавил умножение, + немного переделал вычисление коэфициентов и степеней

--- Task2/PolynomialTest.cs
+++ Task2/PolynomialTest.cs
@@ -9,6 +9,63 @@ namespace Task2
     [TestClass]
     public class PolynomialTest
     {
+        #region коэффициенты и степени полинома для сложения и вычитания
+        double[,] poly1AddSub = new double[,]
+        {
+            { 8, 3 },
+            { 3, 2 },
+            { 5, 1 },
+            { 17, 0 },
+        };
+        double[,] poly2AddSub = new double[,]
+        {
+            { 2, 4 },
+            { 3, 3 },
+            { 4, 2 },
+            { 1, 1 },
+            { -7, 0 }
+        };
+        double[,] poly3AddAssert= new double[,]
+        {
+            { 10, 4 },
+            { 6, 3 },
+            { 9, 2 },
+            { 18, 1 },
+            { -7, 0 }
+        };
+        double[,] poly4SubAssert = new double[,]
+        {
+            { 6, 4 },
+            { 0, 3 },
+            { 1, 2 },
+            { 16, 1 },
+            { -7, 0 }
+        };
+        #endregion
+
+        #region коэффициенты и степени полинома для умножения
+        double[,] poly1multiplie = new double[,]
+        {
+            { 2, 2 },
+            { 2, 1 },
+            { 2, 0 }
+        };
+        double[,] poly2multiplie = new double[,]
+        {
+            { 2, 1 },
+            { 2, 0 }
+        };
+        double[,] polyMultiplieAssert = new double[,]
+        {
+            { 4, 3 },
+            { 4, 2 },
+            { 4, 2 },
+            { 4, 1 },
+            { 4, 1 },
+            { 4, 0 }
+        };
+        #endregion
+
         #region TestContext
         /// <summary>
         /// Контекст для вывода информации в обозревателе тестов
@@ -30,9 +87,9 @@ namespace Task2
         [TestMethod]
         public void AdditionTest()
         {
-            Polynomial p1 = new Polynomial(new double[] { 2, 3, 4, 17 });
-            Polynomial p2 = new Polynomial(new double[] { 2, 3, 4, 1, -7 });
-            Polynomial assert = new Polynomial(new double[] { 4, 6, 8, 18, -7 });
+            Polynomial p1 = new Polynomial(poly1AddSub);
+            Polynomial p2 = new Polynomial(poly2AddSub);
+            Polynomial assert = new Polynomial(poly3AddAssert);
             Polynomial p3 = p1 + p2;
             TestContext.WriteLine(p3.ToString());
             Assert.IsTrue(p3.Equals(assert));
@@ -41,10 +98,21 @@ namespace Task2
         [TestMethod]
         public void SubtractionTest()
         {
-            Polynomial p1 = new Polynomial(new double[] { 3, 3, 4, 17, 4 });
-            Polynomial p2 = new Polynomial(new double[] { 2, 1, 7, 1 });
-            Polynomial assert = new Polynomial(new double[] { 1, 2, -3, 16, 4 });
+            Polynomial p1 = new Polynomial(poly1AddSub);
+            Polynomial p2 = new Polynomial(poly2AddSub);
+            Polynomial assert = new Polynomial(poly4SubAssert);
             Polynomial p3 = p1 - p2;
+            TestContext.WriteLine(p3.ToString());
+            Assert.IsTrue(p3.Equals(assert));
+        }
+
+        [TestMethod]
+        public void MultiplieTest()
+        {
+            Polynomial p1 = new Polynomial(poly1multiplie);
+            Polynomial p2 = new Polynomial(poly2multiplie);
+            Polynomial assert = new Polynomial(polyMultiplieAssert);
+            Polynomial p3 = p1 * p2;
             TestContext.WriteLine(p3.ToString());
             Assert.IsTrue(p3.Equals(assert));
         }
--- Task2Lib/Polynomial.cs
+++ Task2Lib/Polynomial.cs
@@ -8,39 +8,60 @@ namespace Task2Lib
 {
     public class Polynomial
     {
-        private double[] polynomialCoefs;
+        private double[,] polynomialCoefs;
 
-        public Polynomial(double[] polynomialCoefs)
+        public Polynomial(double[,] polynomialCoefs)
         {
             this.polynomialCoefs = polynomialCoefs;
         }
 
-        public double[] PolynomialCoefs
+        public double[,] PolynomialCoefs
         {
             get { return polynomialCoefs; }
+        }
+
+        private double[,] FillDegrees(double[] coefs)
+        {
+            int degree = coefs.Length - 1;
+            double[,] array = new double[coefs.Length, 2];
+
+            for(int i = 0; i < coefs.Length; i++)
+            {
+                array[i, 0] = coefs[i];
+                array[i, 1] = degree;
+                degree--;
+            }
+            return array;
         }
 
         public static Polynomial operator +(Polynomial polynomial1, Polynomial polynomial2)
         {
             Polynomial polynomial3;
             int maxlength;
-            double[] polynomial3coefs;
+            double[,] polynomial3coefs;
 
             if (polynomial1.PolynomialCoefs.Length >= polynomial2.PolynomialCoefs.Length)
-                maxlength = polynomial1.PolynomialCoefs.Length;
+                maxlength = polynomial1.PolynomialCoefs.GetLength(0);
             else
-                maxlength = polynomial2.PolynomialCoefs.Length;
+                maxlength = polynomial2.PolynomialCoefs.GetLength(0);
 
-            polynomial3coefs = new double[maxlength];
+            polynomial3coefs = new double[maxlength, 2];
 
-            for (int i = 0; i < maxlength; i++)
+            for (int i = 0, degree = maxlength - 1; i < maxlength; i++, degree--)
             {
-                if (i >= polynomial1.PolynomialCoefs.Length)
-                    polynomial3coefs[i] = polynomial2.PolynomialCoefs[i];
-                if (i >= polynomial2.PolynomialCoefs.Length)
-                    polynomial3coefs[i] = polynomial1.PolynomialCoefs[i];
-                if (i < polynomial1.PolynomialCoefs.Length && i < polynomial2.PolynomialCoefs.Length)
-                    polynomial3coefs[i] = polynomial1.PolynomialCoefs[i] + polynomial2.PolynomialCoefs[i];
+                if (i >= polynomial1.PolynomialCoefs.GetLength(0))
+                {
+                    polynomial3coefs[i, 0] = polynomial2.PolynomialCoefs[i, 0];
+                }
+                if (i >= polynomial2.PolynomialCoefs.GetLength(0))
+                {
+                    polynomial3coefs[i, 0] = polynomial1.PolynomialCoefs[i, 0];
+                }
+                if (i < polynomial1.PolynomialCoefs.GetLength(0) && i < polynomial2.PolynomialCoefs.GetLength(0))
+                {
+                    polynomial3coefs[i, 0] = polynomial1.PolynomialCoefs[i, 0] + polynomial2.PolynomialCoefs[i, 0];
+                }
+                polynomial3coefs[i, 1] = degree;
             }
 
             polynomial3 = new Polynomial(polynomial3coefs);
@@ -51,34 +72,61 @@ namespace Task2Lib
         {
             Polynomial polynomial3;
             int maxlength;
-            double[] polynomial3coefs;
+            double[,] polynomial3coefs;
 
             if (polynomial1.PolynomialCoefs.Length >= polynomial2.PolynomialCoefs.Length)
-                maxlength = polynomial1.PolynomialCoefs.Length;
+                maxlength = polynomial1.PolynomialCoefs.GetLength(0);
             else
-                maxlength = polynomial2.PolynomialCoefs.Length;
+                maxlength = polynomial2.PolynomialCoefs.GetLength(0);
 
-            polynomial3coefs = new double[maxlength];
+            polynomial3coefs = new double[maxlength, 2];
 
-            for (int i = 0; i < maxlength; i++)
+            for (int i = 0, degree = maxlength - 1; i < maxlength; i++, degree--)
             {
-                if (i >= polynomial1.PolynomialCoefs.Length)
-                    polynomial3coefs[i] = polynomial2.PolynomialCoefs[i];
-                if (i >= polynomial2.PolynomialCoefs.Length)
-                    polynomial3coefs[i] = polynomial1.PolynomialCoefs[i];
-                if (i < polynomial1.PolynomialCoefs.Length && i < polynomial2.PolynomialCoefs.Length)
-                    polynomial3coefs[i] = polynomial1.PolynomialCoefs[i] - polynomial2.PolynomialCoefs[i];
+                if (i >= polynomial1.PolynomialCoefs.GetLength(0))
+                {
+                    polynomial3coefs[i, 0] = polynomial2.PolynomialCoefs[i, 0];
+                }
+                if (i >= polynomial2.PolynomialCoefs.GetLength(0))
+                {
+                    polynomial3coefs[i, 0] = polynomial1.PolynomialCoefs[i, 0];
+                }
+                if (i < polynomial1.PolynomialCoefs.GetLength(0) && i < polynomial2.PolynomialCoefs.GetLength(0))
+                {
+                    polynomial3coefs[i, 0] = polynomial1.PolynomialCoefs[i, 0] - polynomial2.PolynomialCoefs[i, 0];
+                }
+                polynomial3coefs[i, 1] = degree;
             }
 
             polynomial3 = new Polynomial(polynomial3coefs);
             return polynomial3;
         }
 
+        public static Polynomial operator *(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            Polynomial polynomial3;
+            double[,] polynomial3coefs = new double[polynomial1.PolynomialCoefs.GetLength(0) * polynomial2.PolynomialCoefs.GetLength(0), 2];
+            int counter = 0;
+            for (int i = 0; i < polynomial1.PolynomialCoefs.GetLength(0); i++)
+            {
+                for (int j = 0; j < polynomial2.PolynomialCoefs.GetLength(0); j++)
+                {
+                    polynomial3coefs[counter, 0] = polynomial1.PolynomialCoefs[i, 0] * polynomial2.PolynomialCoefs[j, 0];
+                    polynomial3coefs[counter, 1] = polynomial1.PolynomialCoefs[i, 1] + polynomial2.PolynomialCoefs[j, 1];
+                    counter++;
+                }
+            }
+
+            polynomial3 = new Polynomial(polynomial3coefs);
+            return polynomial3;
+        }
+
+
         public bool Equals(Polynomial polynomial)
         {
-            foreach (double item in polynomial.PolynomialCoefs)
+            for(int i = 0; i < polynomial.PolynomialCoefs.GetLength(0); i++)
             {
-                if (!PolynomialCoefs.Contains(item))
+                if (polynomial.PolynomialCoefs[i, 0] != polynomialCoefs[i, 0])
                     return false;
             }
             return true;
@@ -87,27 +135,34 @@ namespace Task2Lib
         public override string ToString()
         {
             StringBuilder builder = new StringBuilder();
-            int degree = 2;
 
-            if (polynomialCoefs.First() < 0)
-                builder.Append(string.Format(@"-{0}x", -polynomialCoefs.First()));
+            if (polynomialCoefs[0, 0] < 0)
+                builder.Append(string.Format(@"-{0}x^{1}", -polynomialCoefs[0, 0], polynomialCoefs[0, 1]));
             else
-                builder.Append(string.Format(@"{0}x", polynomialCoefs.First()));
+                builder.Append(string.Format(@"{0}x^{1}", polynomialCoefs[0, 0], polynomialCoefs[0, 1]));
 
-            for (int i = 1; i < polynomialCoefs.Length - 1; i++)
+            for (int i = 1; i < polynomialCoefs.GetLength(0) - 1; i++)
             {
-                if (polynomialCoefs[i] < 0)
-                    builder.Append(string.Format(" - {0}x^{1}", -polynomialCoefs[i], degree));
+                if (polynomialCoefs[i, 1] == 1)
+                {
+                    if (polynomialCoefs[i, 0] < 0)
+                        builder.Append(string.Format(" - {0}x", -polynomialCoefs[i, 0]));
+                    else
+                        builder.Append(string.Format(" + {0}x", polynomialCoefs[i, 0]));
+                }
                 else
-                    builder.Append(string.Format(" + {0}x^{1}", polynomialCoefs[i], degree));
-                degree++;
+                {
+                    if (polynomialCoefs[i, 0] < 0)
+                        builder.Append(string.Format(" - {0}x^{1}", -polynomialCoefs[i, 0], polynomialCoefs[i, 1]));
+                    else
+                        builder.Append(string.Format(" + {0}x^{1}", polynomialCoefs[i, 0], polynomialCoefs[i, 1]));
+                }
             }
 
-            if (polynomialCoefs.Last() < 0)
-                builder.Append(string.Format(@" - {0}x^{1}", -polynomialCoefs.Last(), degree));
+            if (polynomialCoefs[polynomialCoefs.GetLength(0) - 1, 0] < 0)
+                builder.Append(string.Format(@" - {0}", -polynomialCoefs[polynomialCoefs.GetLength(0) - 1, 0]));
             else
-                builder.Append(string.Format(@" + {0}x^{1}", polynomialCoefs.Last(), degree));
-
+                builder.Append(string.Format(@" + {0}", polynomialCoefs[polynomialCoefs.GetLength(0) - 1, 0]));
             return builder.ToString();
         }
     }
