b4d5954c6a5aecb97e4f49d354a66ecba60b2837
verzhbovsky-alexey <viarzhbouski@gmail.com>
2019-12-21 23:49:14 +0300

    реализовал делегат + событие на отлов сообщения от клиента и сервера

--- Task4Lib/Client.cs
+++ Task4Lib/Client.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task4Lib
 {
     public class Client
     {
+        private Socket clientSocket;
+        private IPEndPoint ipEndPoint;
+
+        public delegate void ToLogMessage(string message, MessageFromEnum messageFrom);
+        public event ToLogMessage LogHandler;
+
+        public Client(string ip, int port)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void InitializeClient()
+        {
+            try
+            {
+                clientSocket.Connect(ipEndPoint);   
+            }
+            catch (SocketException)
+            {
+
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            clientSocket.Send(data);
+            Thread.Sleep(10);
+        }
+
+        public void RecieveMessage()
+        {
+            byte[] data = new byte[256];
+            int bytes = clientSocket.Receive(data);
+            string message = Encoding.Unicode.GetString(data, 0, bytes);
+            LogHandler(message, MessageFromEnum.FromClient);
+        }
     }
 }
--- Task4Lib/Server.cs
+++ Task4Lib/Server.cs
@@ -15,14 +15,16 @@ namespace Task4Lib
         private List<Thread> threads;
         private Socket serverSocket;
         private IPEndPoint ipEndPoint;
- 
+
+        public delegate void ToLogMessage(string message, MessageFromEnum messageFrom);
+        public event ToLogMessage LogHandler;
 
         public Server(int port)
         {
             ipEndPoint = new IPEndPoint(IPAddress.Any, port);
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             clientSockets = new List<Socket>();
-            threads = new List<Thread>();
+            threads = new List<Thread>();          
         }
 
         public void InitializeServer()
@@ -31,10 +33,7 @@ namespace Task4Lib
             {
                 serverSocket.Bind(ipEndPoint);
                 serverSocket.Listen(0);
-                Socket client = serverSocket.Accept();
-                clientSockets.Add(client);
-                Thread thread = new Thread(new ParameterizedThreadStart(SendRecieveMessage));
-                thread.Start(client);
+                serverSocket.BeginAccept(AcceptCallBack, null);
             }
             catch(SocketException)
             {
@@ -42,16 +41,24 @@ namespace Task4Lib
             }
         }
 
-        public void SendRecieveMessage(object client)
+        private void AcceptCallBack(IAsyncResult ar)
         {
-            Socket socket = (Socket)client;
-            StringBuilder message = new StringBuilder();
-            int bytes = 0;
-            byte[] byteMessage = new byte[256];
-            while(true)
+            Socket clientSocket = serverSocket.EndAccept(ar);
+            clientSockets.Add(clientSocket);
+            Thread thread = new Thread(ClientHandler);
+            thread.Start(clientSocket);
+            serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+
+        private void ClientHandler(object socket)
+        {
+            Socket clientSocket = (Socket)socket;
+            while (true)
             {
-                bytes = socket.Receive(byteMessage);
-                message.AppendLine()
+                byte[] data = new byte[256];
+                int bytes = clientSocket.Receive(data);
+                LogHandler(Encoding.Unicode.GetString(data, 0, bytes), MessageFromEnum.FromServer);
+                //clientSocket.Send(data);
             }
         }
     }
