257374ccfa989ac03744c80cec48d62b594c156b
verzhbovsky-alexey <viarzhbouski@gmail.com>
2019-12-21 16:19:03 +0300

    добавил проекты, плюс класс сервера

---
+++ Task4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task4")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88fefb79-a2d7-4216-9ef9-85af321a2f10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task4Lib/Server.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task4Lib
+{
+    public class Server
+    {
+        private List<Socket> clientSockets;
+        private List<Thread> threads;
+        private Socket serverSocket;
+        private IPEndPoint ipEndPoint;
+ 
+
+        public Server(int port)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSockets = new List<Socket>();
+            threads = new List<Thread>();
+        }
+
+        public void InitializeServer()
+        {
+            try
+            {
+                serverSocket.Bind(ipEndPoint);
+                serverSocket.Listen(0);
+                Socket client = serverSocket.Accept();
+                clientSockets.Add(client);
+                Thread thread = new Thread(new ParameterizedThreadStart(SendRecieveMessage));
+                thread.Start(client);
+            }
+            catch(SocketException)
+            {
+
+            }
+        }
+
+        public void SendRecieveMessage(object client)
+        {
+            Socket socket = (Socket)client;
+            StringBuilder message = new StringBuilder();
+            int bytes = 0;
+            byte[] byteMessage = new byte[256];
+            while(true)
+            {
+                bytes = socket.Receive(byteMessage);
+                message.AppendLine()
+            }
+        }
+    }
+}
